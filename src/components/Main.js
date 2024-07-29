d2c6feb9c7be8b97cdc6fa76a46fc5e857f713c0
beebrown <bruno.grosdesirs@outlook.fr>
2024-07-29 23:08:27 +0200

    maj

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,28 +1,31 @@
 import Yoga from '../assets/yoga.svg';
-import Run from '../assets/run.svg';
-import Swim from '../assets/swim.svg';
-import Bike from '../assets/bike.svg';
+import Dumbbell from '../assets/dumbbell.svg';
+import Swim from '../assets/swimming.svg';
+import Bike from '../assets/bicycle.svg';
 
 function Main() {
   return (
     <div className="main">
-
-      <div className="activity">
-        <div className="activity__icon">
-          <img src={Yoga} alt="yoga" />
-        </div>
-        <div className="activity__icon">
-          <img src={Run} alt="run" />
-        </div>
-        <div className="activity__icon">
-          <img src={Swim} alt="swim" />
-        </div>
-        <div className="activity__icon">
-          <img src={Bike} alt="bike" />
+      <div className='activity__menu'>
+        <div className="activity">
+          <div className="activity__icon">
+            <img src={Yoga} alt="yoga" />
+          </div>
+          <div className="activity__icon">
+            <img src={Swim} alt="Swim" />
+          </div>
+          <div className="activity__icon">
+            <img src={Bike} alt="bike" />
+          </div>
+          <div className="activity__icon">
+            <img src={Dumbbell} alt="Bumbbell" />
+          </div>
+          <div className="copyright">
+            <p>Copyright, SportSee 2020</p>
+          </div>
         </div>
       </div>
       <div className="summary">
-
       </div>
     </div>
   );
